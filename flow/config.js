98dd4ec4fe5fdfca2439c6fe51e0db282bd9e038
Clay Ewing <c.ewing@miami.edu>
2022-06-07 12:01:15 -0500

    REF Pray Function

---
+++ flow/config.js
@@ -0,0 +1,6 @@
+import { config } from "@onflow/fcl";
+
+config({
+ "accessNode.api": "https://rest-mainnet.onflow.org", //Testnet: "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
+  "discovery.wallet": "Mainnet: "https://fcl-discovery.onflow.org/authn", "//Testnet: "https://fcl-discovery.onflow.org/testnet/authn" // Mainnet: "https://fcl-discovery.onflow.org/authn"
+})
