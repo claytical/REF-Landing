6dcce60eb1da3d70c52a425bc8f4fcb7ac1d16d0
Clay Ewing <c.ewing@miami.edu>
2022-06-08 09:57:20 -0500

    update config

--- flow/config.js
+++ flow/config.js
@@ -2,5 +2,5 @@ import { config } from "@onflow/fcl";
 
 config({
  "accessNode.api": "https://rest-mainnet.onflow.org", //Testnet: "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
-  "discovery.wallet": "Mainnet: "https://fcl-discovery.onflow.org/authn", "//Testnet: "https://fcl-discovery.onflow.org/testnet/authn" // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/authn"// "//Testnet: "https://fcl-discovery.onflow.org/testnet/authn"
 })
